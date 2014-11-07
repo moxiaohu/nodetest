58c8c92f8c6c61a1caf81cc7c7d8ebd897db7c2c
moxiaohu <xiaohumo@xiaohumodeMacBook-Pro.local>
2014-11-07 16:27:54 +0100

    change port
    change port

--- hichatdiy/server.js
+++ hichatdiy/server.js
@@ -7,7 +7,7 @@ var users =[];
 
 app.use('/', express.static(__dirname + '/www'));
 
-server.listen(3000);
+server.listen(8080);
 
 io.sockets.on('connection', function(socket) {
     //new user login
